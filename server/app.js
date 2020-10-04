5b19533b997760bb63c000aa124ba005dd50c203
Lucas <o.lucas.lima@gmail.com>
2020-10-04 19:38:45 -0300

    Generating random Clients

--- server/app.js
+++ server/app.js
@@ -7,21 +7,30 @@ const Client = require('./models/Client');
 
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost:27017/kaiji', {
+mongoose.connect('mongodb+srv://admin:admin@pipedrivertobling.xtpd1.gcp.mongodb.net/clients?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 app.use(morgan('tiny'))
 app.use(bodyParser.json());
-
-app.get("/clients", function(req,res){
+Client.find({}).then(async clients=>{
+    const random = require('random-name')
+    if(clients.length === 0){
+        for(let i=0; i<10; i++){
+            let newClient = new Client({firstName:random.first(), lastName: random.last()})
+            await newClient.save()
+        }
+    }
+}).catch(err=>{
+    console.log("err")
+})
+app.get("/client", function(req,res){
     Client.find({}).then(clients=>{
         res.status(200).json({clients})
     }).catch(err=>{
         res.status(500).send("Problem accessing db")
     })
 })
-
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
